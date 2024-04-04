09c4b8bdfd712227bd44f015012f0c16d51b6999
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-04-04 13:30:46 +0900

    db_backup2

--- backup.sh
+++ backup.sh
@@ -1,8 +1,10 @@
 # 백업을 저장할 디렉토리 경로
-BACKUP_DIR=/usr/src/app/db
+BACKUP_DIR=./backup/
+
+mkdir -p $BACKUP_DIR
 
 # 현재 날짜를 백업 파일 이름으로 사용
 BACKUP_FILE="$BACKUP_DIR/db_backup_$(date +'%Y%m%d').sql.gz"
 
 # Docker 컨테이너에서 PostgreSQL 백업 실행
-docker exec db pg_dump -U poko_db_user_prod -d poko_db_prod | gzip > $BACKUP_FILE
+sudo docker exec db pg_dump -U poko_db_user_prod -d poko_db_prod | gzip > $BACKUP_FILE
