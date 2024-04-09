06ba50fd53683dcc7a8fea94bf8f8b99b21aee88
Ubuntu <ubuntu@ip-172-26-12-134.ap-northeast-2.compute.internal>
2024-04-09 06:58:34 +0000

    product server commit

--- poko/settings.py
+++ poko/settings.py
@@ -152,6 +152,6 @@ AUTH_USER_MODEL = "auth.User"
 
 
 # 배포시 설정(csrf토큰 설정), 개발환경에서는 admin login issue로 사용하지 말 것
-# CSRF_TRUSTED_ORIGINS = ["https://www.hellopoko.com", "https://hellopoko.com"]
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
+ CSRF_TRUSTED_ORIGINS = ["https://www.hellopoko.com", "https://hellopoko.com"]
+ CSRF_COOKIE_SECURE = True
+ SESSION_COOKIE_SECURE = True
