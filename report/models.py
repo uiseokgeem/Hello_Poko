c16d5f0a73c346e66356dfe28f457660e730275a
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-05-29 15:29:31 +0900

    2024년 5월 29일 UI 반영 필드 옵션 수정 완료

--- report/models.py
+++ report/models.py
@@ -11,14 +11,24 @@ class MemberCheck(models.Model):
         on_delete=models.CASCADE,
         to_field="name",
     )
+    gqs = models.BooleanField(default=True)  # GQS 참석 여부
     status = models.CharField(max_length=500, null=True, default=None)
     pray_member = models.CharField(max_length=300, null=True, default=None)
     date = models.DateTimeField()
 
 
 class UserCheck(models.Model):
-    worship_choice = [(1, "1부 예배"), (2, "2부 예배"), (3, "3부 예배"), (0, "불참")]
-    qt_choice = [(1, "1회"), (2, "2회"), (3, "3회"), (4, "4회"), (5, "5회"), (0, "0회")]
+    worship_choice = [(0, "불참"), (1, "1부 예배"), (2, "2부 예배"), (3, "3부 예배")]
+    qt_choice = [
+        (0, "0회"),
+        (1, "1회"),
+        (2, "2회"),
+        (3, "3회"),
+        (4, "4회"),
+        (5, "5회"),
+        (6, "6회"),
+        (7, "7회"),
+    ]
 
     teacher = models.ForeignKey(
         User,
@@ -38,9 +48,7 @@ class UserCheck(models.Model):
         default=None,
         choices=qt_choice,
     )
-    meeting = models.CharField(max_length=3, null=True, default=None)  # 예/아니오
-
-    pray_time = models.CharField(max_length=4, null=True, default=None)  # 001분
+    meeting = models.BooleanField(default=True)  # PM: 참석여부를 default=True로 하기 원함
     pray_youth = models.CharField(
         max_length=300, null=True, default=None
     )  # 청소년부를 위한 기도
@@ -48,5 +56,8 @@ class UserCheck(models.Model):
         max_length=300, null=True, default=None
     )  # 반 모임을 위한 기도
     pray_user = models.CharField(max_length=300, null=True, default=None)  # 사용자에 대한 기도
-    pray_emergency = models.CharField(max_length=300, null=True, default=None)  # 긴급 기도
+    issue = models.IntegerField(max_length=300, null=True, default=None)  # 문의사항/긴급사항
     date = models.DateTimeField()  # 작성일 기준 이전 주일 입력
+
+    # pray_emergency = models.CharField(max_length=300, null=True, default=None)  # 교사 긴급 기도 보류
+    # pray_time = models.CharField(max_length=4, null=True, default=None)  # 교사 사용자 기도 시간 보류
