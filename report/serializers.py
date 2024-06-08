a4a828ae3fdf7dfd73b67686af602278270b638a
김의석 <119546939+uiseokgeem@users.noreply.github.com>
2024-06-08 16:38:05 +0900

    feat : 양육일지 타이틀 조회 구현을 위해 UserCheck에 title 필드 추가 및 TitleListSerializer 와 TitleListAPIView 구현 완료

--- report/serializers.py
+++ report/serializers.py
@@ -8,9 +8,10 @@ class TitleListSerializer(serializers.ModelSerializer):
         model = UserCheck
         fields = [
             "id",
+            "teacher",
             "title",
         ]
 
-    @staticmethod  # -> requset 객체 접근이 필요할 경우 인스턴스 메서드로 전환, 이번 실습에서는 정적 메서드 유지하며 requst 객체에 전달.
+    @staticmethod
     def get_optimized_queryset(request) -> QuerySet[UserCheck]:
         return UserCheck.objects.filter(teacher=request.user).only("id", "title")
--- report/views.py
+++ report/views.py
@@ -9,8 +9,6 @@ from rest_framework.utils.serializer_helpers import ReturnDict
 class TitleListAPIView(ListAPIView):
     serializer_class = TitleListSerializer
 
-    # queryset = TitleListSerializer.get_optimized_queryset -> 쿼리셋이 requset.user에 접근하기 위해
-    # get_queryset을 오버라이드하여  request를 인자로 받는 인스턴스 메서드로 재정의 함
     def get_queryset(self):
         return TitleListSerializer.get_optimized_queryset(self.request)  # self?
 
